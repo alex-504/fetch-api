36eb00b180dd82dfab00b1baa274723f9cc7814e
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2021-01-25 12:54:02 +0900

    refactored v2

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 fetch('https://jsonplaceholder.typicode.com/users')
-.then(response => response.json()) // one line only!
-
-//convert this response to json -->  // *promise
-// if promise fulfills
+.then(response => response.json())
 .then(data => {
   console.log(data);
 })
-
-// if the promise throws error
 .catch(error => {
   console.log('There was an error, error');
 })
