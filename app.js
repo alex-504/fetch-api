7367c72df5ca0228a418bf26e8ff4b510e5618c3
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2021-01-25 11:55:22 +0900

    Create first request

---
+++ app.js
@@ -0,0 +1,20 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(checkStatus)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log('There was an error', error);
+  });
+
+
+function checkStatus(response) {
+  if (response.ok) {
+      return response;
+  }
+
+  let error = new Error(response.statusText);
+  error.response = response;
+  return Promise.reject(error);
+}
