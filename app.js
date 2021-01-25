6eb98c83ace25e5cef0462bd67839a615364e5e0
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2021-01-25 12:39:50 +0900

    fetch data using json

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 fetch('https://jsonplaceholder.typicode.com/users')
+.then(function(response) {
+  return response.json(); //convert this response to json -->  // promise
+})
+.then(function (data) {
+  console.log(data);
+})
